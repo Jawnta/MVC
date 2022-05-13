76daf77543daa52f20da9f21bccb81011dfe9871
Jawnta <jawntalol@gmail.com>
2022-05-13 18:29:20 +0200

    more dockblocks

--- src/Card/Dealer.php
+++ src/Card/Dealer.php
@@ -4,11 +4,17 @@ namespace App\Card;
 
 use JetBrains\PhpStorm\Pure;
 
+/**
+ * Dealer class
+ */
 class Dealer
 {
+
     public array $hand;
     public int $score;
-
+    /**
+     * construct for dealer which holds the properties for dealer hand and dealer score.
+     */
     public function __construct($hand = [], $score = 0)
     {
         $this->hand = $hand;
