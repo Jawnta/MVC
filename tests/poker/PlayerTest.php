7ee9786857f3099aa6de5d0e529c050ce3122d2f
Jawnta <jawntalol@gmail.com>
2022-05-21 12:28:48 +0200

    tests for player class

--- tests/poker/PlayerTest.php
+++ tests/poker/PlayerTest.php
@@ -27,24 +27,7 @@ class PlayerTest extends TestCase
 
         $this->assertEquals(1950, $player->balance);
     }
-//    public function rePickCards(Request $request, Session $session)
-//    {
-//        $rule = New Rules();
-//        $deck = $session->get('pokerDeck');
-//        $player = $session->get('pokerPlayer');
-//        foreach ($request->request->keys() as $card) {
-//            $index = $this->getCardIndexByImgPath($card, $player);
-//            array_splice($player->hand, $index, 1);
-//            $player->hand[] = array_pop($deck);
-//
-//        }
-//
-//        $player->replaced = true;
-//        $player->currentHand = $rule->evaluateAndGetHand($session, "pokerPlayer");
-//
-//        $session->set('pokerPlayer', $player);
-//        $session->set('pokerDeck', $deck);
-//    }
+
     public function testPlayerRePickCards()
     {
         $session = new Session(new MockArraySessionStorage());
@@ -59,16 +42,49 @@ class PlayerTest extends TestCase
         $player->hand = $array;
         $session->set('pokerPlayer', $player);
 
-        $request = new Request(["two_of_hearts_svg"]);
+        $request = new Request([["two_of_hearts_svg"]]);
         $card = [new Card("hearts", 14, "ace")];
         $session->set('pokerDeck', $card);
 
         $player->rePickCards($request, $session);
 
         $player = $session->get('pokerPlayer');
-
         $this->assertEquals(true, $player->replaced);
 
     }
+
+    public function testPlayerSetBet()
+    {
+        $session = new Session(new MockArraySessionStorage());
+        $player = new Player();
+        $session->set('pokerPlayer', $player);
+
+        $player->setBet($session, 50);
+        $this->assertEquals(50, $player->bet);
+    }
+
+    public function testPlayerGetBet()
+    {
+        $session = new Session(new MockArraySessionStorage());
+        $player = new Player();
+        $player->bet = 100;
+        $session->set('pokerPlayer', $player);
+
+        $bet = $player->getBet($session);
+
+        $this->assertEquals(100, $bet);
+    }
+
+    public function testPlayerMakeBet(){
+        $session = new Session(new MockArraySessionStorage());
+        $player = new Player();
+        $session->set('pokerPlayer', $player);
+        $player->makeBet($session, 500);
+
+        $this->assertEquals(500, $player->bet);
+        $this->assertEquals(1500, $player->balance);
+
+    }
+
 
 }
